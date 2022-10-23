50f96ba2e7e820562ae87e36f880609610ef9b25
Martin Estrada <84754369+mestra185@users.noreply.github.com>
2022-10-22 23:16:20 -0700

    Done

---
+++ script.js
@@ -0,0 +1,85 @@
+var selectedRow = null
+
+function onFormSubmit(e) {
+	event.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
+}
+
+//Retrieve the data
+function readFormData() {
+    var formData = {};
+    formData["productCode"] = $("#productCode").val();
+    formData["product"] = $("#product").val();
+    formData["qty"] = $("#qty").val();
+    formData["perPrice"] = $("#perPrice").val();
+    return formData;
+    console.log(formData)
+}
+
+//Insert the data
+function insertNewRecord(data) {
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.productCode;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.product;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.qty;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.perPrice;
+    cell4 = newRow.insertCell(4);
+        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+
+}
+
+//Edit the data
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.productCode;
+    selectedRow.cells[1].innerHTML = formData.product;
+    selectedRow.cells[2].innerHTML = formData.qty;
+    selectedRow.cells[3].innerHTML = formData.perPrice;
+}
+
+//Delete the data
+function onDelete(td) {
+    if (confirm('Do you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+       
+    }
+}
+
+//Reset the data
+function resetForm() {
+    
+    var pCode = $("#productCode").val()
+    pCode = ""
+
+    var prod = $("#product").val()
+    prod = ""
+
+    var q = $("qty").val()
+    q = ""
+
+    var price = $("#perPrice").val()
+    price = ""
+    
+
+    selectedRow = null;
+}
+
